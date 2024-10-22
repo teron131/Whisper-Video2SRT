67388482c91316a494c36675a1055417b6282dac
teron131 <t3ron.ay@gmail.com>
2024-10-22 08:45:34 +0000

    Rework

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ The Whisper Video2SRT repository focuses on converting video files (formats supp
 
 # Usage
 
-1.  Place your video files into the `video_input` folder.
+1.  Place your video files into the `input` folder. (Create if not exist)
 
-2.  Run the `video2srt.ipynb` or `video2srt.py` script.
+2.  Run the `video2srt.py` script.
+
+3.  It runs on the last modified video file in the `input` folder and outputs the audio file and srt file in the `output` folder.
 
 # Requirements
 
@@ -16,11 +18,13 @@ The Whisper Video2SRT repository focuses on converting video files (formats supp
 
 1. **Install Homebrew** (if not already installed):
    Run this in Terminal:
+
    ```
    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
    ```
 
 2. **Add Homebrew to PATH** (if needed):
+
 - For Zsh (default shell):
   ```
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zshrc
@@ -32,20 +36,23 @@ The Whisper Video2SRT repository focuses on converting video files (formats supp
   ```
 
 3. **Install FFmpeg**:
-    ```
-    brew install ffmpeg
-    ```
+   ```
+   brew install ffmpeg
+   ```
 
 ### On Windows:
 
 1. **Download FFmpeg**:
+
 - Visit [FFmpeg's official website](https://ffmpeg.org/download.html).
 - Choose and download the Windows build.
 
 2. **Extract and Place FFmpeg**:
+
 - Extract the downloaded file to a folder, for example, `C:\\FFmpeg`.
 
 3. **Add FFmpeg to PATH**:
+
 - Type "Environment Variables" in the search bar on the Start menu.
 - Click on "Edit the system environment variables" or "Edit environment variables for your account".
 - Under 'System Variables', find and select the 'Path' variable, then click 'Edit'.
@@ -57,6 +64,7 @@ Remember to restart your IDE after installing FFmpeg.
 ## Install Python Packages
 
 Run this command:
+
 ```
 pip install -r requirements.txt
 ```
--- video2srt.py
+++ video2srt.py
@@ -1,170 +1,187 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 import os
 import shutil
-import sys
+import subprocess
 import warnings
+from pathlib import Path
+from typing import Dict
 
-import opencc
+import fal_client
 import torch
-from tqdm import tqdm
+from opencc import OpenCC
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 
 warnings.filterwarnings("ignore")
 
+os.environ["FAL_KEY"] = ""
 
-# # Folders Creation
+# Constants
+INPUT_DIR = "input/"
+OUTPUT_DIR = "output/"
+TARGET_LANG = "zh"
 
-
-def get_base_path():
-    if getattr(sys, "frozen", False):
-        # Running in a bundle (executable created by PyInstaller)
-        return os.path.dirname(sys.executable)
-    elif "__file__" in globals():
-        # Running in a script or packaged environment
-        return os.path.dirname(os.path.abspath(__file__))
-    else:
-        # Running in a Jupyter notebook
-        return os.getcwd()
+# Ensure directories exist
+os.makedirs(INPUT_DIR, exist_ok=True)
+os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 
-base_path = get_base_path()
-
-video_dir = base_path + "/video_input/"
-audio_dir = base_path + "/audio_output/"
-srt_dir = base_path + "/srt_output/"
-source_lang = "chinese"
-
-os.makedirs(audio_dir, exist_ok=True)
-os.makedirs(srt_dir, exist_ok=True)
+# Subtitle preprocessing functions
+s2hk = OpenCC("s2hk").convert
 
 
-# # Video to Audio: FFmpeg
-
-
-def extract_audio_from_video(video_dir, audio_dir):
-    """
-    Extracts audio from any file in the specified directory using ffmpeg. If the file is not a video, ffmpeg will return an error. The extracted audio files are saved in FLAC format in another directory.
-
-    Args:
-    - video_dir (str): Directory containing the input files.
-    - audio_dir (str): Directory where the extracted audio files will be saved.
-
-    Returns:
-    - None
-    """
-
-    # Delete the directory and its contents if the directory exists
-    if os.path.exists(audio_dir):
-        shutil.rmtree(audio_dir)
-
-    # Create the output directory if it doesn't exist
-    if not os.path.exists(audio_dir):
-        os.makedirs(audio_dir)
-
-    video_files = [f for f in os.listdir(video_dir) if f != ".DS_Store"]
-    for filename in tqdm(video_files, desc="Extracting audio"):
-        print(filename)
-        video_file_path = os.path.join(video_dir, filename)
-        audio_file_name = os.path.splitext(filename)[0] + ".flac"
-        audio_file_path = os.path.join(audio_dir, audio_file_name)
-
-        # Convert video to audio using ffmpeg
-        os.system(f'ffmpeg -y -i "{video_file_path}" -q:a 0 -map a "{audio_file_path}" -hide_banner -loglevel error')
-
-        print(f"Extracted audio from video: {filename}")
-
-
-extract_audio_from_video(video_dir=video_dir, audio_dir=audio_dir)
-
-
-# # Transcription: Whisper
-
-
-def convert_time_to_srt(seconds_float):
-    """Converts a time in seconds to 'hh:mm:ss,ms' format for SRT."""
+# Utility functions
+def convert_time_to_hms(seconds_float: float) -> str:
     hours, remainder = divmod(seconds_float, 3600)
     minutes, seconds = divmod(remainder, 60)
-    seconds, milliseconds = divmod(seconds, 1)
-    milliseconds = int(milliseconds * 1000)
+    milliseconds = int((seconds % 1) * 1000)
     return f"{int(hours):02}:{int(minutes):02}:{int(seconds):02},{milliseconds:03}"
 
 
-def whisper_transcribe(input_directory, output_directory):
-    # Load HuggingFace Whisper model
-    device = "cuda:0" if torch.cuda.is_available() else ("mps" if torch.backends.mps.is_available() else "cpu")
+def response_to_srt(result: Dict, srt_path: str) -> None:
+    with open(srt_path, "w", encoding="utf-8") as srt_file:
+        for counter, chunk in enumerate(result["chunks"], 1):
+            start_time = chunk.get("timestamp", [0])[0]
+            end_time = chunk.get("timestamp", [0, start_time + 2.0])[1]
+            start_time_hms = convert_time_to_hms(start_time)
+            end_time_hms = convert_time_to_hms(end_time)
+            transcript = s2hk(chunk["text"].strip())
+            srt_entry = f"{counter}\n{start_time_hms} --> {end_time_hms}\n{transcript}\n\n"
+            srt_file.write(srt_entry)
+
+
+def extract_audio_from_video(video_file_path: Path) -> bool:
+    print(f"Extracting audio from: {video_file_path}")
+    filename = video_file_path.stem
+    audio_file_path = Path(f"{OUTPUT_DIR}/{filename}").with_suffix(".mp3")
+
+    try:
+        subprocess.run(
+            ["ffmpeg", "-y", "-i", str(video_file_path), "-q:a", "0", "-map", "a", str(audio_file_path), "-hide_banner"],
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+        print(f"Audio extracted successfully: {audio_file_path}")
+        return True
+    except subprocess.CalledProcessError as e:
+        print(f"Error extracting audio from {video_file_path}:")
+        print(f"ffmpeg stdout: {e.stdout}")
+        print(f"ffmpeg stderr: {e.stderr}")
+        return False
+
+
+# Transcribe
+
+
+def whisper_hf_transcribe(audio_path: str):
+    """
+    Transcribe audio file using whisper-large-v3-turbo model with Hugging Face optimization.
+
+    Returns:
+        dict: A dictionary containing the transcription result with the following structure:
+            {
+                "text": str,  # Full transcribed text
+                "chunks": [
+                    {
+                        "timestamp": Tuple[float],  # Start and end time of the chunk
+                        "text": str,  # Transcribed text for this chunk
+                    }
+                ]
+            }
+    """
+    device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
     torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
     print(f"Using device: {device} ({torch_dtype})")
 
-    model_id = "openai/whisper-large-v3"
-
+    model_id = "openai/whisper-large-v3-turbo"
     model = AutoModelForSpeechSeq2Seq.from_pretrained(
         model_id,
         torch_dtype=torch_dtype,
         low_cpu_mem_usage=True,
         use_safetensors=True,
-    )
-    model.to(device)
+    ).to(device)
     processor = AutoProcessor.from_pretrained(model_id)
 
-    # Setup pipeline
     pipe = pipeline(
         "automatic-speech-recognition",
         model=model,
         tokenizer=processor.tokenizer,
         feature_extractor=processor.feature_extractor,
-        max_new_tokens=128,
-        chunk_length_s=30,
         batch_size=16,
         return_timestamps=True,
         torch_dtype=torch_dtype,
         device=device,
     )
-
-    # Process audio files
-    audio_files = [file for file in os.listdir(input_directory) if file.endswith(".flac")]  # Adjust the extension if needed
-    for audio_file in tqdm(audio_files, desc="Transcribing audio files"):
-        audio_path = os.path.join(input_directory, audio_file)
-        result = pipe(audio_path, return_timestamps=True, generate_kwargs={"language": "chinese"})  # Adjust language if needed
-
-        # Process and write SRT content
-        srt_content = []
-        counter = 1
-        for chunk in result["chunks"]:
-            start_time_srt = convert_time_to_srt(chunk["timestamp"][0])
-
-            # Assign a default duration if the end timestamp is None
-            if chunk["timestamp"][1] is not None:
-                end_time_srt = convert_time_to_srt(chunk["timestamp"][1])
-            else:
-                # Assuming a default duration of 2 seconds
-                end_time_srt = convert_time_to_srt(chunk["timestamp"][0] + 2.0)
-
-            transcript = chunk["text"].strip()
-            srt_content.append(f"{counter}\n{start_time_srt} --> {end_time_srt}\n{transcript}")
-            counter += 1
-
-        srt_output = "\n\n".join(srt_content)
-        srt_filename = os.path.splitext(audio_file)[0] + ".srt"
-        srt_path = os.path.join(output_directory, srt_filename)
-
-        # Convert simplified Chinese to traditional Chinese
-        converter = opencc.OpenCC("s2hk")
-        srt_output = converter.convert(srt_output)
-
-        # Save to SRT file
-        with open(srt_path, "w", encoding="utf-8") as srt_file:
-            srt_file.write(srt_output)
-
-        print(f"Generated SRT: {srt_filename}")
+    result = pipe(audio_path)
+    return result
 
 
-whisper_transcribe(input_directory=audio_dir, output_directory=srt_dir)
+def whisper_fal_transcribe(audio_path: str, language: str = "en") -> Dict:
+    """
+    Transcribe an audio file using fal-ai/wizper model.
+
+    This function uploads the audio file, subscribes to the transcription service,
+    and returns the transcription result.
+
+    It defaults at English.
+
+    Args:
+        audio_path (str): The path to the audio file to be transcribed.
+        language (str): The language of the audio file. Defaults to "en".
+    Returns:
+        dict: A dictionary containing the transcription result with the following structure:
+            {
+                "text": str,  # Full transcribed text
+                "chunks": List[dict],  # List of transcription chunks
+                    # Each chunk is a dictionary with:
+                    {
+                        "timestamp": List[float],  # Start and end time of the chunk
+                        "text": str,  # Transcribed text for this chunk
+                    }
+            }
+    """
+
+    def on_queue_update(update):
+        if isinstance(update, fal_client.InProgress):
+            for log in update.logs:
+                print(log["message"])
+
+    url = fal_client.upload_file(audio_path)
+    result = fal_client.subscribe(
+        # "fal-ai/wizper",
+        "fal-ai/whisper",
+        arguments={
+            "audio_url": url,
+            "task": "transcribe",
+            "language": language,
+        },
+        with_logs=True,
+        on_queue_update=on_queue_update,
+    )
+    return result
 
 
-# # Delete Extra Files
+# Main
 
-# Delete the directory and its contents if the directory exists
-if os.path.exists(audio_dir):
-    shutil.rmtree(audio_dir)
+
+def process_video(video_file_path: Path) -> None:
+    if extract_audio_from_video(video_file_path):
+        audio_file_path = Path(f"{OUTPUT_DIR}/{video_file_path.stem}").with_suffix(".mp3")
+        result = whisper_fal_transcribe(audio_path=str(audio_file_path), language=TARGET_LANG)
+        # result = whisper_hf_transcribe(audio_path=str(audio_file_path))
+        response_to_srt(result=result, srt_path=audio_file_path.with_suffix(".srt"))
+    else:
+        print(f"Skipping transcription for {video_file_path} due to audio extraction failure.")
+
+
+if __name__ == "__main__":
+    if os.path.exists(OUTPUT_DIR):
+        shutil.rmtree(OUTPUT_DIR)
+    os.makedirs(OUTPUT_DIR)
+
+    video_files = [Path(f"{INPUT_DIR}/{f}") for f in os.listdir(INPUT_DIR) if f != ".DS_Store"]
+    if not video_files:
+        print("No video files found in the input directory.")
+    else:
+        latest_video = max(video_files, key=os.path.getmtime)
+        print(f"Processing the most recently modified video: {latest_video}")
+        process_video(latest_video)
